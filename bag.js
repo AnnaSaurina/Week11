de0e780dec18f9a4b10d4678d6a473c671f885f4
AnnaSaurina <the_happy_ann@mail.ru>
2023-02-22 21:44:55 +0300

    Added js+css

---
+++ bag.js
@@ -0,0 +1,8 @@
+function plus() {
+    let product1 = document.getElementsByClassName("item4");
+    let product2 = document.getElementsByClassName("item8");
+    let product3 = document.getElementsByClassName("item12");
+    let product4 = document.getElementsByClassName("item16");
+
+    let sum(Number(product1) + Number(product2) + Number(product3) + Number(product4));
+};

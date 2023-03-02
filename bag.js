5fc9cd7b18ad48a3261b711f3acd999ba8aeb6d2
AnnaSaurina <the_happy_ann@mail.ru>
2023-03-02 21:59:02 +0300

    Added function recalc

--- bag.js
+++ bag.js
@@ -1,8 +1,19 @@
-function plus() {
-    let product1 = document.getElementsByClassName("item4");
-    let product2 = document.getElementsByClassName("item8");
-    let product3 = document.getElementsByClassName("item12");
-    let product4 = document.getElementsByClassName("item16");
+let product1 = document.getElementById("item4").innerText;
+let product2 = document.getElementById("item8").innerText;
+let product3 = document.getElementById("item12").innerText;
+let product4 = document.getElementById("item16").innerText;
 
-    let sum(Number(product1) + Number(product2) + Number(product3) + Number(product4));
+function plus() {
+    let summ = document.getElementById("sum");
+    let sum = (Number(product1) + Number(product2) + Number(product3) + Number(product4));
+    summ.innerHTML = 'Итого ' + sum;
+};
+
+plus();
+
+function recalc() {
+    let summ = document.getElementById("sum");
+    let  numberSumm = summ.innerText.split(' ');
+    let newSumm = Number(numberSumm[1]) * 0.8;
+    summ.innerHTML = 'Итого ' + newSumm;
 };
